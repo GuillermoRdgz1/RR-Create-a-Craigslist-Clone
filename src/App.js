e46ad5e17146aa14e5b20f10de1801ae39253dce
guillermo rodriguez <guillermordgz.96@gmail.com>
2023-02-15 20:26:17 -0800

    Importing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 // Import data
 import { postings } from "./postings";
 // Import components
-import './App.css';
+import "./App.css";
+import Gallery from "./Components/Gallery";
+import Directory from "./Components/Directory";
+import Searchbar from "./Components/Searchbar";
+import Sidebar from "./Components/Sidebar";
+import Help from "./Components/Help";
+
+
 
 function App(){
   return (
     <div>
       <h1>craigslist</h1>
       <div className="App">
-        {/* Your content will go here! */}
+        <Searchbar />
+        <Directory />
+        <Gallery postings={postings} />
+        <Sidebar />
+        <Help />
       </div>
     </div>
   );
---
+++ src/Components/Directory.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Directory = (props) => {
+    <div className='directory'>
+        <h1>Directory</h1>
+    </div>
+}
+
+export default Directory
